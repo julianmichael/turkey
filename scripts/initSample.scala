72bb37798de316c6e4fd69f3559b0bed01bb04c3
Julian Michael <julianjohnmichael@gmail.com>
2017-09-21 14:45:00 -0700

    Add example convenience functions to initSample script

--- scripts/initSample.scala
+++ scripts/initSample.scala
@@ -1,14 +1,57 @@
 import turkey._
 import turkey.tasks._
 import turkey.sample._
+import com.amazonaws.services.mturk.model._
+import com.amazonaws.services.mturk._
 val hitDataService = new InMemoryHITDataService
 implicit val config = SandboxTaskConfig(
   "turkey-sample",
-  "localhost",
+  "nlp.cs.washington.edu",
   hitDataService)
 val exp = new SampleExperiment
+
+def yesterday = {
+  val cal = java.util.Calendar.getInstance
+  cal.add(java.util.Calendar.DATE, -1)
+  cal.getTime
+}
+
+import scala.collection.JavaConverters._
+
+def expireHITById(hitId: String) = {
+  config.service.updateExpirationForHIT(
+    (new UpdateExpirationForHITRequest)
+      .withHITId(hitId)
+      .withExpireAt(yesterday))
+}
+
+def approveAllAssignmentsByHITId(hitId: String) = for {
+  mTurkAssignment <- config.service.listAssignmentsForHIT(
+    new ListAssignmentsForHITRequest()
+      .withHITId(hitId)
+      .withAssignmentStatuses(AssignmentStatus.Submitted)
+    ).getAssignments.asScala.toList
+} yield config.service.approveAssignment(
+  new ApproveAssignmentRequest()
+    .withAssignmentId(mTurkAssignment.getAssignmentId)
+    .withRequesterFeedback(""))
+
+def deleteHITById(hitId: String) =
+  config.service.deleteHIT((new DeleteHITRequest).withHITId(hitId))
+
+def disableHITById(hitId: String) = {
+  expireHITById(hitId)
+  deleteHITById(hitId)
+}
+
+def getActiveHITIds = {
+  config.service.listHITs(new ListHITsRequest).getHITs.asScala.toList.map(_.getHITId)
+}
+
 def exit = {
+  // remove everything from the sandbox because it won't persist anyway
+  exp.disable
+
   config.actorSystem.terminate
-  // TODO exit the console without killing SBT... how?
 }
 exp.server
